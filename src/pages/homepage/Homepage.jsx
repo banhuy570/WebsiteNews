71fb3309fc81f996f70fe39cf22bc17a0f468864
banhuy570 <71781815+banhuy570@users.noreply.github.com>
2021-12-24 09:16:27 +0700

    1

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,14 +1,10 @@
-// import { useLocation } from "react-router";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./homepage.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
-// import { useParams } from "react-router-dom";
-
 export default function Homepage() {
-  // const params = useParams();
   const [newsArray, setNewsArray] = useState([]);
   const [newsResults, setNewsResults] = useState();
   const [loadMore, setLoadMore] = useState(10);
@@ -35,11 +31,7 @@ export default function Homepage() {
       (newsItem) => newsItem.title.toLowerCase().indexOf(searchTitle) > -1
     );
   }
-  // function searchCategory(news) {
-  //   return news.filter(
-  //     (newsItem) => newsItem.category.toLowerCase().indexOf(searchTitle) > -1
-  //   );
-  // }
+
   return (
     <>
       <Header></Header> 
